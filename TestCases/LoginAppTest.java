56993fbaa4f4ccc8e80da5800eaec8ca61d85b31
Fareeha <fareehank584@gmail.com>
2024-12-01 02:48:57 +0500

    save file

--- TestCases/LoginAppTest.java
+++ TestCases/LoginAppTest.java
@@ -7,19 +7,6 @@ import static org.junit.Assert.*;
 //this is software testing assignment 3
 
 public class LoginAppTest {
-
-    @Test
-    public void testValidLogin() throws Exception {
-        // Test Case 1: Valid Login with Correct Email
-        LoginApp loginApp = new LoginApp();
-        Method method = LoginApp.class.getDeclaredMethod("authenticateUser", String.class);
-        method.setAccessible(true);
-
-        String userName = (String) method.invoke(loginApp, "johndoe@example.com");
-        assertNotNull("User should be authenticated with a valid email.", userName);
-        assertEquals("John Doe", userName);
-    }
-
     @Test
     public void testCaseSensitivity() throws Exception {
         // Test Case 2: Case Sensitivity Check
